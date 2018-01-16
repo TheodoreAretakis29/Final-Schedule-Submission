f62fd41becbc6d33db988608e9cf8f55c61d5388
TheodoreAretakis29 <theodore.aretakis19@alumni.loyola.ca>
2018-01-16 09:18:33 -0500

    Add files via upload

---
+++ Schedule.js
@@ -0,0 +1,16 @@
+document.getElementById()
+var A = "Mr. Miles-run";
+var B = "Mr. Jimbo";
+var C = "Mr. Lafontaine";
+var D = "Fr. Diaspora";
+var E = "Mr. Floyd";
+var F = "Mrs. Quadratics";
+var G = "Mme. French";
+var H = "Ms. Poe";
+var I = "Mr. Moose";
+var J = "Mrs. Gaudi";
+var K = "Mr. Mars";
+var L = "Mr. Fulderio";
+var M = "Late Start";
+var N = "Mass or Level Assembly";
+var O = "Early Dissmissal";
