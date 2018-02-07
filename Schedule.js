c0cc29f768f3682415aa29e5f644eaf5d7ce94d1
TheodoreAretakis29 <theodore.aretakis19@alumni.loyola.ca>
2018-02-07 10:55:56 -0500

    Add files via upload

--- Schedule.js
+++ Schedule.js
@@ -1,16 +1,45 @@
-document.getElementById()
-var A = "Mr. Miles-run";
-var B = "Mr. Jimbo";
-var C = "Mr. Lafontaine";
-var D = "Fr. Diaspora";
-var E = "Mr. Floyd";
-var F = "Mrs. Quadratics";
-var G = "Mme. French";
-var H = "Ms. Poe";
-var I = "Mr. Moose";
-var J = "Mrs. Gaudi";
-var K = "Mr. Mars";
-var L = "Mr. Fulderio";
-var M = "Late Start";
-var N = "Mass or Level Assembly";
-var O = "Early Dissmissal";
+function myFunction() {
+    var A = document.getElementsByClassName("gym");
+    var i;
+    for (i = 0; i < A.length; i++) {
+        A[i].style.backgroundColor = "#F0F8FF";
+    }
+	
+	
+	
+	
+	
+switch (courses) {
+    case 'french':
+		console.log("french is the secondary language leanrt.");
+        break;
+    case 'science':
+		console.log("Science is a helpful course, and involves thinking outside the box.");
+        break;
+    case 'english':
+		console.log("English is an intense course, requiring one to analyse and understand what they are reading.")
+        break;
+    case 'math':
+		console.log("Math is good, math is great, on a math test i gtot an 88");
+        break;
+    case 'computer prog.':
+		console.log("Computer programming is the reason this schedule exists." );
+        break;
+    case 'architecture'
+		console.log("The second option course, architecture teahces us about the various different styles. ");
+        break;
+    case 'speech':
+		console.log("Speech pushes students out of their comfort zone, enabling them to become confident public speakers.");
+		break;
+	case 'history':
+		console.log("The history course is my personal favourite, teaching students about our candian heritage.");
+		break;
+	case 'reigion':
+		console.log("The secondary 4 religion course involves learning about the history of the church.");
+		break;
+	case 'early'
+		console.log("Time to go home early!!!");
+	case 'level assembly'
+		console.log("A waste of time");
+		
+}
