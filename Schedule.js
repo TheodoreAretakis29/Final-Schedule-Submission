89b088c649a53880fc1d0c09280c1b05ca338797
TheodoreAretakis29 <theodore.aretakis19@alumni.loyola.ca>
2018-02-13 15:08:01 -0500

    Add files via upload

--- Schedule.js
+++ Schedule.js
@@ -1,42 +1,56 @@
+function Classrooming ()
 var COURSESPLACE = document.getElementById("Classroom").value;
 
-switch (COURSEPLACE) {
+switch (COURSESPLACE) {
     case 'french':
 		console.log("french is the secondary language leanrt.");
+			var DVD = "French";
         break;
     case 'science':
 		console.log("Science is a helpful course, and involves thinking outside the box.");
+			var DVD = "science";
         break;
     case 'english':
 		console.log("English is an intense course, requiring one to analyse and understand what they are reading.");
+			var DVD = "english";
         break;
     case 'math':
 		console.log("Math is good, math is great, on a math test i gtot an 88");
+			var DVD = "math";
         break;
     case 'computer prog.':
 		console.log("Computer programming is the reason this schedule exists." );
+			var DVD = "comoputer prog.";
         break;
     case 'architecture':
 		console.log("The second option course, architecture teahces us about the various different styles. ");
+			var DVD = "architecture";
         break;
     case 'speech':
 		console.log("Speech pushes students out of their comfort zone, enabling them to become confident public speakers.");
+			var DVD = "speech";
 		break;
 	case 'history':
 		console.log("The history course is my personal favourite, teaching students about our candian heritage.");
+			var DVD = "history";
 		break;
 	case 'religion':
 		console.log("The secondary 4 religion course involves learning about the history of the church.");
+			var DVD = "religion";
 		break;
 	case 'early':
 		console.log("Time to go home early!!!");
+			var DVD = "early";
 	case 'level assembly':
 		console.log("A waste of time");
+			var DVD = "level assembly";
 		break;
 	case 'late start':
 		console.log("We can sleep in!");
+			var DVD = "late start";
 		break;
 	case 'homeroom':
-		console.log("Will we be arranging a bake sale? Nobody knows.")
+		console.log("Will we be arranging a bake sale? Nobody knows.");
+			var DVD = "homeroom";
 }	
-		
+	var Monado = 
